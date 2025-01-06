839b2b8d3060113468435cb731839452a9279579
Piberius <cestlly.stash@gmail.com>
2025-01-06 22:09:25 +0800

    Push IDE specific files and lib folder

--- src/com/main/MyFirstJavaProject.java
+++ src/com/main/MyFirstJavaProject.java
@@ -8,8 +8,8 @@ import com.main.process.NumberToWordsServiceWSDL;
 public class MyFirstJavaProject {
 
 	public static void main(String[] args) {
-//		FruitService.getFruits(); //Sample Marshaller
-//		NumberToWordsServiceWSDL.numberToWords(); //Numbers to Words using online generated WSDL
+		FruitService.getFruits(); //Sample Marshaller
+		NumberToWordsServiceWSDL.numberToWords(); //Numbers to Words using online generated WSDL
 		CountryInfoService.getCountryCode(); // Get Country Code from Country Name
 	}
 
